465bc506a5d7f7c599e87b02549ce315fdfdb12b
ThePratikGyawali <whoispratik@gmail.com>
2024-09-17 19:01:16 -0400

    comit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
-import AxiosTutorial from './AxiosTutorial.js'
+import PersonList from './components/PersonList';
+import PersonAdd from './components/PersonAdd';
+import PersonRemove from './components/PersonRemove';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div ClassName="App">
+      <PersonAdd/>
+      <PersonList/>
+      <PersonRemove/>
     </div>
-  );
+  )
 }
-
-export default App;
